5a436fefcedaac865b1a09b6d47efdd8c6c54dc5
shuangzou6 <54931322+shuangzou6@users.noreply.github.com>
2019-09-10 21:23:48 +0800

    review1
    logic error

--- Tic_tac_toe.cpp
+++ Tic_tac_toe.cpp
@@ -2,33 +2,33 @@ bool isWin(char game[3][3])
 {
 
     bool win = false;
-
-    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O'))
+// no need to judge the role of player
+    if (game[0][0] == game[0][1] && game[0][1] == game[0][2])
         win = true;
 
-    if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O'))
+    if (game[1][0] == game[1][1] && game[1][1] == game[1][2] )
         win = true;
 
-    if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O'))
+    if (game[2][0] == game[2][1] && game[2][1] == game[2][2])
         win = true;
 
     // column
 
-    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O'))
+    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] )
         win = true;
 
-    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O'))
+    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] )
         win = true;
 
-    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O'))
+    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] )
         win = true;
 
     // diagonal
 
-    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O'))
+    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] )
         win = true;
 
-    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O'))
+    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] )
         win = true;
 
     return win;
